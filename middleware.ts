e183c0c7a8bca78af0e74aba9be98229a322ab2b
raptor7197 <vamsikrishna75836@gmail.com>
2026-06-28 01:51:21 +0530

    fix: middlaware depreciation issues

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export const runtime = "edge";
-
 const protectedPaths = [
   "/dashboard",
   "/fellowship/application",
